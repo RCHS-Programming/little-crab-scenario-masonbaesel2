699bb1dda78966dc2fdd7b5288ea4df1c89e6113
masonbaesel2 <75792384+masonbaesel2@users.noreply.github.com>
2021-03-23 08:37:38 -0400

    made the crab have a different animation and added a score board on to the screen.

--- Crab.java
+++ Crab.java
@@ -7,6 +7,7 @@ public class Crab extends Actor
 {
     private GreenfootImage image1;
     private GreenfootImage image2;
+    private int wormsEaten;
     
     /*  
     * Create a crab and initialize its two images.  
@@ -17,10 +18,13 @@ public class Crab extends Actor
         image1 = new GreenfootImage ("crab.png");
         image2 = new GreenfootImage ("crab2.png");
         setImage (image1);
+        wormsEaten = 0;
     }
     public void act()
     {
+        
         checkKeyPress();
+        switchImage();
         move();
         lookForWorm();
         
@@ -40,6 +44,16 @@ public class Crab extends Actor
         {
             removeTouching(Worm.class);
             Greenfoot.playSound ("au.wav");
+            wormsEaten += 1;
+            
+            getWorld().showText("Score: " + wormsEaten, 50, 500);
+            if (wormsEaten == 10)
+            {
+            Greenfoot.playSound ("fanfare.wav");Greenfoot.stop();
+            }
+        }
+        
+        {
         }
     }
     
@@ -54,7 +68,18 @@ public class Crab extends Actor
             turn (4);
         }
     }
-   
+    //switch image of the crab to make it look animated
+    public void switchImage()
+    {
+        if (getImage() == image1)
+        {
+            setImage (image2);
+        }
+        else
+        {
+            setImage (image1);
+        }
+    }
 }
     
   
